51b27a2deaf71165913a0576069374fa8b587a19
chris-vl <tlhnok@gmail.com>
2017-01-07 23:19:03 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-forum_auth = {
-        'forum_url': "forum_link",#where forum_link your forum link
-        'username': "username", #where username your username
-        'password': "password"  #where pass your password
-    }
